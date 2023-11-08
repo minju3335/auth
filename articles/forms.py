bfb80cc4e9a854b0eaa92b7381fc918fa879665a
minju <kimminju3335@naver.com>
2023-11-08 16:16:54 +0900

    드디어 완성

--- articles/forms.py
+++ articles/forms.py
@@ -2,6 +2,21 @@ from django import forms
 from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
+    # title = forms.CharField(
+    #     label='제목입니다.',
+    #     widget=forms.TextInput(
+    #         attrs={'class':'form-control'}
+
+    #     )
+    # )
+
+    # content = forms.CharField(
+    #     label='내용',
+    #     widget=forms.TextArea(
+    #         attrs={'class':'form-control'}
+    #     )
+    # )
+
     class Meta:
         model = Article
         # fields = '__all__'
@@ -10,6 +25,12 @@ class ArticleForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        # label = '내용',
+        widget=forms.Textarea(
+            attrs={"cols":"40", "rows":"1"},
+        )
+    )
     class Meta:
         model = Comment
         fields = ('content',)
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ArticleForm, CommentForm
-from .models import Article
+from .models import Article, Comment
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -8,19 +8,19 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     articles = Article.objects.all()
     context = {
-        'articles':articles
+        'articles':articles,
     }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 @login_required
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
-        if form.is_valie():
+        if form.is_valid():
             article = form.save(commit=False)
             article.user = request.user
-            artcle.save()
-            return redirect('article:index')
+            article.save()
+            return redirect('articles:index')
     else:
         form = ArticleForm()
 
@@ -44,17 +44,61 @@ def detail(request, id):
     return render(request, 'detail.html', context)
 
 
+@login_required
+def delete(request, id):
+    article = Article.objects.get(id=id)
+
+    if request.user == article.user:
+        article.delete()
+
+    return redirect('articles:index')
+
+
+@login_required
+def update(request, id):
+    article = Article.objects.get(id=id)
+
+    if request.user != article.user:
+        return redirect('articles:detail', id=id)
+    if request.method == 'POST':
+        # 기존 정보를 덮어씌우기 위한 것
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail',id=id)
+    else:
+        # 기존 정보를 보여주기 위한 것
+        form = ArticleForm(instance=article)
+
+    context = {
+        'form': form,
+    }
+        
+    return render(request, 'form.html', context)
+
+
+
+@login_required
 def comment_create(request, article_id):
     form = CommentForm(request.POST)
 
     if form.is_valid():
         comment = form.save(commit=False)
-        comment.user = request.user
-        comment.article = article
+        # comment.user = request.user
+        # comment.article = article
 
         comment.user_id = request.user.id
         comment.article_id = article_id
         
         comment.save()
 
-        return redirect('articles:detail', id=article.id)
+        return redirect('articles:detail', id=article_id)
+
+
+def comment_delete(request, article_id, id):
+    comment = Comment.objects.get(id=id)
+    
+    if request.user == comment.user:
+        comment.delete()
+
+    return redirect('articles:detail', id=article_id)
